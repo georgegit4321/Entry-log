02ed52a844af63d6652e362144caa7ec079c4838
George Sebastian <georgesebastian4321@gmail.com>
2024-06-28 11:05:06 +0530

    login added

--- app/src/main/java/com/example/entrylog/MainActivity.java
+++ app/src/main/java/com/example/entrylog/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.entrylog;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    AppCompatButton b1;
+    EditText ed1,ed2;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        ed1=(EditText) findViewById(R.id.ed1);
+        ed2=(EditText) findViewById(R.id.ed2);
+        b1=(AppCompatButton) findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1 = "",s2 = "";
+                try {
+                    s1 = ed1.getText().toString();
+                    s2 = ed2.getText().toString();
+                    Toast.makeText(getApplicationContext(),s1+"  "+s2,Toast.LENGTH_SHORT).show();
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(),"invalid",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
